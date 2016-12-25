4b67a4932ca627d26ff1f2103a5673190b1049d7
vec <vec@moebaka.com>
2016-12-25 16:02:12 +0800

    new test

--- test/pam-event.js
+++ test/pam-event.js
@@ -2,8 +2,7 @@ const should = require('should');
 
 const PamEventEmitter = require('../admin/src/pam-event.js');
 
-describe('event emit', () => {
-	const pe = new PamEventEmitter;
+let test = (pe) => {
 	let value = 0;
 	const f = function (set) {
 		value = set;
@@ -16,6 +15,29 @@ describe('event emit', () => {
 			.length(1)
 			.containEql(f);
 	});
+	it('bind multi Handle', () => {
+		let i = 0;
+		const fnList = [
+			() => ++i,
+			() => ++i,
+			() => ++i
+		];
+		pe.on('fnList', ...fnList);
+		pe.emit('fnList');
+
+		i.should.equal(3);
+	});
+	it('bind multi Event', () => {
+		let i = 0;
+		const fn = () => ++i;
+		const evList = ['fn1', 'fn2', 'fn3', 'fn4'];
+
+		pe.on(evList, fn);
+		evList.forEach(evName => pe.emit(evName));
+
+		i.should.equal(4);
+	})
+
 	it('emit a event', () => {
 		const arg = 9;
 		pe.emit('test', arg);
@@ -44,4 +66,15 @@ describe('event emit', () => {
 			.is.an.Array()
 			.length(0)
 	})
+};
+
+describe('直接在构造函数的实例中使用', () => {
+	const pe = new PamEventEmitter;
+	test(pe);
 });
+
+describe('在其他对象中使用', () => {
+	const bindPe = {};
+	PamEventEmitter.use(bindPe);
+	test(bindPe);
+})
