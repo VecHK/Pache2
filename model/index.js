83c644845ae73160fa5ba41b2ff743725954b2a6
vec <vec@moebaka.com>
2016-12-28 20:50:00 +0800

    new model connect

--- model/index.js
+++ model/index.js
@@ -1,28 +1,20 @@
 const mongoose = require('mongoose');
 const envir = require('../envir');
 
-require('./article');
-
-mongoose.connect(envir.db, {
-	server: { poolSize: 20 },
-	function (err) {
-		if (err) {
-			console.error('mongodb 连接错误: ', envir.db, err.message);
-			setTimeout(process.exit, 9, -1);
-			throw err;
-		}
-		console.info('mongodb connected: ', envir.db);
-	}
+const connectStatus = new Promise(function (resolve, reject) {
+	mongoose.connect(envir.db, {
+		server: { poolSize: 20 },
+	}, function (err) {
+		if (err) { reject(err) }
+		else { resolve() }
+	});
 });
 
-Object.assign(exports, {
+let model = {
 	Article: require('./article') && mongoose.model('Article'),
-	removeCollection: function (name, cb) {
-		mongoose.connection.db.dropCollection(name, (err, result) => {
-			if (err) {
-				throw err;
-			}
-			cb(result);
-		});
-	},
-});
+	removeCollection: mongoose.connection.db.dropCollection.bind(mongoose.connection.db),
+	mongoose,
+	connectStatus
+};
+
+module.exports = model;
