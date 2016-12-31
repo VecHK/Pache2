b30ff4a29e9f668b1a439ff8f0506de088674a70
vec <vec@moebaka.com>
2017-01-01 02:06:19 +0800

    setEnvir

--- test/envir.js
+++ test/envir.js
@@ -1,17 +1,26 @@
-const envir = require('../envir')
-const should = require('should');
 
-describe('Envir', () => {
-	it('get', (done) => {
-		/* envir.reload(); */
-		envir.get('port', function (value) {
-			should(value).equal(envir.port);
+const should = require('should');
+const cluster = require('cluster');
+
+if (cluster.isMaster) {
+	describe('Envir Master', () => {
+		const envir = require('../envir')
+		it('setEnvir', done => {
+			const workers = [cluster.fork()]
+			envir.setEnvir(workers);
 			done();
 		})
-	});
-	it('使用错误的文件路径', () => {
-		envir.CONFIG_PATH = './不存在的文件名';
+		it('get', (done) => {
+			/* envir.reload(); */
+			envir.get('port', function (value) {
+				should(value).equal(envir.port);
+				done();
+			})
+		});
+		it('使用错误的文件路径', () => {
+			envir.CONFIG_PATH = './不存在的文件名';
 
-		(() => envir.reload()).should.throw(`Envir 初始化失败，请检查 ${envir.CONFIG_PATH} 是否存在，或者检查 Suc 语法是否正确`)
+			(() => envir.reload()).should.throw(`Envir 初始化失败，请检查 ${envir.CONFIG_PATH} 是否存在，或者检查 Suc 语法是否正确`)
+		})
 	})
-})
+}
