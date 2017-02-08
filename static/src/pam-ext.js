e9e393f5fb66cfe3d0b32efea4c93cae1b080f1d
vec <vec@moebaka.com>
2017-02-09 05:28:01 +0800

    split-page 加入 split-layer 修改了一些样式（内容框背景颜色，字体颜色等），同时也兼容了 split-page 文章页进入特效（渐入） 文章样式微调

--- static/src/pam-ext.js
+++ static/src/pam-ext.js
@@ -3,6 +3,7 @@
 	Pache Article Page Extend Collection
 
  - split-layer
+ - split-page
 
  */
 var 适配间隙 = 0;
@@ -16,7 +17,7 @@ var objExt = function (source, newobj){
 
 class SplitLayerFootnote {
 	collectFootnote(){
-		var pre = $('sup.footnote-ref', this.article);
+		var pre = $('sup.footnote-ref', this.articleEle);
 
 		return pre.map(ele => {
 			let a = ele.getElementsByTagName('a');
@@ -69,26 +70,33 @@ class Layer {
 			//clearEmptyNode(a);
 		});
 	}
-	position(footnote = this.footnote){
+	position(footnote = this.footnote, cb){
 		const {a, sup} = footnote;
 		const {contentFrame} = this;
 
 		let lineHeight = Number(getComputedStyle(sup.parentNode, null).lineHeight.replace(/px$/g, ''));
 
-		contentFrame.style.top = (sup.offsetTop + lineHeight * 0.6) + 'px';
+		contentFrame.style.top = (sup.offsetParent.offsetTop + sup.offsetTop + lineHeight * 0.6) + 'px';
 
 		contentFrame.style.height = (contentFrame.scrollHeight) + 'px';
 		a.style.height = (contentFrame.scrollHeight + lineHeight * 1) + 'px';
 
 		sup.style.lineHeight = (contentFrame.scrollHeight + lineHeight * 1) + 'px';
+
+		let time = Number(getComputedStyle(this.contentFrame, null).transitionDuration.replace(/s$/g, ''));
+		setTimeout(() => {
+			this.emit('opened', this);
+		}, time * 1000)
 	}
 	open(footnote = this.footnote){
 		this.switch = true;
+		this.emit('open', this);
 		this.container.style.display = 'block';
 		this.position(footnote)
 	}
 	close(footnote = this.footnote){
 		this.switch = false;
+
 		this.contentFrame.style.height = '0px';
 		this.footnote.a.style.height = '';
 		this.footnote.sup.style.lineHeight = '';
@@ -98,16 +106,25 @@ class Layer {
 
 		let time = Number(getComputedStyle(this.contentFrame, null).transitionDuration.replace(/s$/g, ''))
 		setTimeout(() => {
+			this.emit('close', this);
 			this.container.style.display = 'none';
 			backObj.cb && backObj.cb();
 		}, time * 1000)
 
 		return backObj;
 	}
+	toggle(){
+		return this[this.switch ? 'close' : 'open'](...arguments)
+	}
 	setCloseEvent(){
 		this.bg.onclick = e => {
-			this.close();
+			if (!this.lock) {
+				this.close()
+			}
 		};
+	}
+	rePosition(cb){
+		return this.switch && this.close().ok(this.open.bind(this))
 	}
 	setResize(){
 		let checkSize = (() => {
@@ -122,7 +139,7 @@ class Layer {
 			};
 		})();
 		window.addEventListener('resize', e => {
-			this.switch && checkSize() && this.close().ok(this.open.bind(this))
+			checkSize() && this.rePosition()
 		})
 	}
 
@@ -150,27 +167,32 @@ class Layer {
 		this.setResize();
 	}
 }
+PamEventEmitter.use(Layer.prototype)
+
 class SplitLayer extends SplitLayerFootnote {
 	getAnthorContent(footnote){
-		var anthor = $$(`[href="#${footnote.a.id}"]`, this.article);
+		var anthor = $$(`[href="#${footnote.a.id}"]`, $$('.footnotes'));
 		return anthor.parentNode.innerHTML;
 	}
 	setAction(footnotes = this.footnotes){
 		footnotes.forEach(footnote => {
-			if (footnote.sup.previousElementSibling && footnote.sup.previousElementSibling.tagName.toLowerCase() === 'h-hws') {
-				$(footnote.sup.previousElementSibling).remove()
+			const supPreEle = footnote.sup.previousElementSibling;
+			if (supPreEle && supPreEle.tagName.toLowerCase() === 'h-hws') {
+				$(supPreEle).remove()
 			}
-			if (footnote.sup.nextElementSibling && footnote.sup.nextElementSibling.tagName.toLowerCase() === 'h-hws') {
-				footnote.sup.nextElementSibling.remove()
+			const supNextEle = footnote.sup.nextElementSibling;
+			if (supNextEle && supNextEle.tagName.toLowerCase() === 'h-hws') {
+				supNextEle.remove()
 			}
 
 			const layer = new Layer(footnote);
 			footnote.layer = layer;
 
 			layer.setContent(this.getAnthorContent(footnote));
-
+			layer.on('open', () => { layer.lock = true })
+			layer.on('opened', () => { layer.lock = false })
 			footnote.a.onclick = e => {
-				layer.open(footnote);
+				layer.lock || layer.toggle(footnote)
 				return false;
 			};
 		})
@@ -181,4 +203,158 @@ class SplitLayer extends SplitLayerFootnote {
 	}
 }
 
-let splitLayer = new SplitLayer(document.getElementById('article'))
+class SplitPage {
+	throwNoContainer(){
+		throw new Error('splitPage 初始化失败：没有指定容器')
+	}
+	init(container = this.throwNoContainer()){
+		this.container = container;
+		this.page = [];
+		this.current = 0;
+	}
+
+	viewerTopMoreThanArticle(scrollableElement = document.body){
+		return scrollableElement.scrollTop > this.container.offsetTop;
+	}
+
+	getSplitElements(){
+		return $('.split-page', this.container).filter(ele => ele.parentNode === this.container)
+	}
+
+	createSplitPage(){
+		let containerHTML = this.container.innerHTML;
+		this.container.innerHTML = '';
+
+		(() => {
+			if (this.splitElements.length) {
+				console.log(containerHTML.split(this.splitElements[0].outerHTML))
+				console.info(containerHTML);
+				console.warn(this.splitElements[0].outerHTML);
+				return containerHTML.split(this.splitElements[0].outerHTML)
+			} else {
+				return [containerHTML]
+			}
+		})()
+		.map((html, cursor, totalHtml) => {
+			if (html.length) {
+				let pageEle = document.createElement('div');
+				pageEle.classList.add('page');
+				pageEle.innerHTML = html;
+
+				let scrollTop = $$('article').offsetTop;
+				window.addEventListener('resize', e => {
+					scrollTop = $$('article').offsetTop
+				})
+
+				let nextBtn = document.createElement('button');
+				$(nextBtn).text('下一页');
+				nextBtn.classList.add('next');
+				nextBtn.classList.add('page-btn');
+				nextBtn.onclick = e => {
+					++this.current;
+
+					console.log(this.page, this.current, this.page[this.current])
+					if (!this.viewerTopMoreThanArticle()) {
+						this.page[this.current].style.position = 'absolute'
+					} else {
+						this.page[this.current].style.position = ''
+					}
+					this.page[this.current].style.display = 'block';
+					setTimeout(() => {
+						this.page[this.current].classList.add('current-page')
+
+						setTimeout(() => {
+							console.warn(scrollTop)
+							if (this.viewerTopMoreThanArticle()) {
+								scrollTo(document.body, scrollTop)
+							}
+							this.page[this.current].classList.add('solid-page')
+							pageEle.classList.remove('solid-page')
+							pageEle.classList.remove('current-page')
+							pageEle.style.display = 'none'
+							this.page[this.current].style.position = ''
+						}, 700)
+					}, 100)
+				};
+
+				let previousBtn = document.createElement('button');
+				$(previousBtn).text('上一页');
+				previousBtn.classList.add('previous');
+				previousBtn.classList.add('page-btn');
+				previousBtn.onclick = e => {
+					--this.current;
+					this.page[this.current].style.display = 'block';
+					setTimeout(() => {
+						this.page[this.current].classList.add('current-page')
+						pageEle.classList.remove('current-page')
+						setTimeout(() => {
+							scrollTo(document.body, scrollTop)
+							pageEle.classList.remove('solid-page')
+							this.page[this.current].classList.add('solid-page')
+							pageEle.style.display = 'none';
+						}, 700)
+					}, 100)
+				};
+				/* 翻页按钮栏 */
+				const pageBtnPanel = document.createElement('section')
+				pageBtnPanel.classList.add('page-btn-panel')
+				/* 非第一页 */
+				if (cursor) {
+					pageBtnPanel.appendChild(previousBtn)
+				}
+				/* 非最后一页 */
+				if ((totalHtml.length - 1) !== cursor) {
+					pageBtnPanel.appendChild(nextBtn)
+				}
+				pageEle.appendChild(pageBtnPanel)
+
+				this.page.push(pageEle);
+				if (this.page.length === 1) {
+					this.show(pageEle)
+				} else {
+					this.rebound(pageEle)
+				}
+				let pageEleHan = Han(pageEle).render()
+				this.container.appendChild(pageEle)
+				setTimeout(() => {
+					const splitLayer = new SplitLayer(pageEle)
+				}, 100);
+			}
+		})
+	}
+	/* 弹出 page 元素 */
+	rebound(pageEle){
+		pageEle.classList.remove('solid-page')
+		pageEle.classList.remove('current-page')
+		let time = Number(getComputedStyle(pageEle, null).transitionDuration.replace(/s/g))
+		setTimeout(() => {
+			pageEle.style.display = 'none'
+		}, time * 1000)
+	}
+
+	/* 显示 page 元素 */
+	show(pageEle){
+		pageEle.style.display = 'block';
+		pageEle.classList.add('current-page')
+		pageEle.classList.add('solid-page')
+	}
+	position(){}
+	split(){
+		this.splitElements = this.getSplitElements()
+		this.createSplitPage()
+	}
+}
+
+class FrontFrame {
+	constructor(){
+		let article = $$('#article');
+		let sp = new SplitPage;
+		sp.init(article);
+		sp.split();
+		this.sp = sp;
+		console.log(sp);
+	}
+}
+
+let ff = new FrontFrame;
+document.body.style.opacity = '';
